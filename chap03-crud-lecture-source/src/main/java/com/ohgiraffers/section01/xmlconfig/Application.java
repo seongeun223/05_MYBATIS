bdd02a7e0d05392c8e4d9446261459f040b8aedd
seongeun223 <jklun080921@gmail.com>
2024-06-20 12:51:46 +0900

    xmlconfig

--- chap03-crud-lecture-source/src/main/java/com/ohgiraffers/section01/xmlconfig/Application.java
+++ chap03-crud-lecture-source/src/main/java/com/ohgiraffers/section01/xmlconfig/Application.java
@@ -32,9 +32,9 @@ public class Application {
             switch (no) {
                 case 1: menuController.selectAllMenu(); break;
                 case 2: menuController.selectMenuByCode(inputMenuCode()); break;
-                case 3: break;
-                case 4: break;
-                case 5: break;
+                case 3: menuController.registMenu(inPutMenu()); break;
+                case 4: menuController.modifyMenu(inputModifyMenu()); break;
+                case 5: menuController.deleteMenu(inputMenuCode());break;
                 default:
                     System.out.println("잘못된 메뉴를 선택하셨습니다.");
                     break;
@@ -49,6 +49,43 @@ public class Application {
 
         Map<String, String> parameter = new HashMap<>();
         parameter.put("code", code);
+
+        return parameter;
+    }
+
+    private static Map<String, String> inPutMenu() {
+       Scanner sc = new Scanner(System.in);
+        System.out.println("메뉴 이름을 입력하세요. : ");
+        String name = sc.nextLine();
+        System.out.println("메뉴 가격을 입력하세요. : ");
+        String price = sc.nextLine();
+        System.out.println("카테고리 코드를 입력하세요. : ");
+        String categoryCode = sc.nextLine();
+
+        Map<String, String> parameter = new HashMap<>();
+        parameter.put("name", name);
+        parameter.put("price", price);
+        parameter.put("categoryCode", categoryCode);
+
+        return parameter;
+    }
+    private static Map<String, String> inputModifyMenu() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("수정할 메뉴 코드를 입력하세요. : ");
+        String code = sc.nextLine();
+        System.out.println("수정할 메뉴 이름을 입력하세요. : ");
+        String name = sc.nextLine();
+        System.out.println("수정할 메뉴 가격을 입력하세요. : ");
+        String price = sc.nextLine();
+        System.out.println("수정할 카테고리 코드를 입력하세요. : ");
+        String categoryCode = sc.nextLine();
+
+        Map<String, String> parameter = new HashMap<>();
+        parameter.put("code", code);
+        parameter.put("name", name);
+        parameter.put("price", price);
+        parameter.put("categoryCode", categoryCode);
 
         return parameter;
     }
